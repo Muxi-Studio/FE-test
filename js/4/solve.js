9c0742f4bc2a6d7bc934d620c525d20632a41f0d
lty2025 <3640830372@qq.com>
2025-11-22 16:36:11 +0800

    Implement student management functions

--- js/4/solve.js
+++ js/4/solve.js
@@ -17,6 +17,7 @@ function createGradeManager() {
      * @param {Object} student
      */
     addStudent(student) {
+        students.push(student);
     },
 
     /**
@@ -24,6 +25,11 @@ function createGradeManager() {
      * @param {"math"|"english"} subject
      */
     getAverage(subject) {
+        let total = 0;
+        for (const student of students) {
+            total += student[subject];
+        }
+        return students.length ? total / students.length : 0;
     },
 
     /**
@@ -32,6 +38,22 @@ function createGradeManager() {
      * @returns {Array} 排序后的新数组
      */
     getRankList(subject) {
+        for(let i=0;i<students.length;i++){
+            for(let j=0;j<students.length-i-1;j++){
+                if(students[j][subject]<students[j+1][subject]){
+                    let temp=students[j];
+                    students[j]=students[j+1];
+                    students[j+1]=temp;
+                }
+            }
+        }
+        for(let i=0;i<students.length;i++){
+            students[i].rank=i+1;
+            students[i].score=students[i][subject];
+            delete students[i].math;
+            delete students[i].english;
+        }
+        return [...students];
     },
 
     /**
@@ -39,13 +61,14 @@ function createGradeManager() {
      * @param {string} keyword
      */
     search(keyword) {
+        return students.filter(student => student.name.includes(keyword));
     },
 
     /**
      * 获取全部学生（返回副本）
      */
     getAll() {
-      
+      return [...students];
     },
   };
 }
