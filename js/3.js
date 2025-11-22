c2f6e925e0212cea5f983fe10e81065ee9028de5
ashes1452 <16416236+ashes1452@user.noreply.gitee.com>
2025-11-22 17:10:22 +0800

    add

--- js/3.js
+++ js/3.js
@@ -5,4 +5,25 @@
  */
 function hasStraightFlush(arr) {
     // 返回 true 或 false
+    let newarr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newarr.push(arr[i] % 13);
+    }
+    let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let arr2 = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+    let arr3 = [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38];
+    let arr4 = [39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr1.includes(arr[i])) {
+            arr1.splice(arr1.indexOf(arr[i] % 13), 1);
+        } else if (arr2.includes(arr[i])) {
+            arr2.splice(arr2.indexOf(arr[i] % 13), 1);
+        } else if (arr3.includes(arr[i])) {
+            arr3.splice(arr3.indexOf(arr[i] % 13), 1);
+
+        } else if (arr4.includes(arr[i])) {
+            arr4.splice(arr4.indexOf(arr[i] % 13), 1);
+        }
+
+    }
 }
--- js/4/solve.js
+++ js/4/solve.js
@@ -17,6 +17,12 @@ function createGradeManager() {
      * @param {Object} student
      */
     addStudent(student) {
+      if (!student || typeof student !== 'object') return;
+      const { name, math, english } = student;
+      if (typeof name !== 'string' || typeof math !== 'number' || typeof english !== 'number') {
+        return;
+      }
+      students.push({ name, math, english });
     },
 
     /**
@@ -24,6 +30,24 @@ function createGradeManager() {
      * @param {"math"|"english"} subject
      */
     getAverage(subject) {
+      let average = 0;
+      if (subject === 'math') {
+        let sum = 0;
+        for (const student of students) {
+          sum += student[subject];
+        }
+        average = students.length ? sum / students.length : 0;
+        return average;
+
+      }
+      if (subject === 'english') {
+        let sum = 0;
+        for (const student of students) {
+          sum += student[subject];
+        }
+        average = students.length ? sum / students.length : 0;
+        return average;
+      }
     },
 
     /**
@@ -32,6 +56,7 @@ function createGradeManager() {
      * @returns {Array} 排序后的新数组
      */
     getRankList(subject) {
+
     },
 
     /**
@@ -45,7 +70,7 @@ function createGradeManager() {
      * 获取全部学生（返回副本）
      */
     getAll() {
-      
+
     },
   };
 }
