651f17f3b8b19e0888527c8327b302cebd19b48b
lty2025 <3640830372@qq.com>
2025-11-22 16:37:01 +0800

    Refactor flatten function to use reduce method

--- js/1.js
+++ js/1.js
@@ -4,6 +4,9 @@
  * @returns {any[]}
  */
 function flatten(arr) {
-  
+  let result = arr.reduce((acc, val) =>{
+    return acc.concat(Array.isArray(val) ? flatten(val) : val);
+  }, []);
+  return result;
 }
-console.log(flatten([[1,2,3,4],[1,[1,[1]]]])==[1,2,3,4,1,1,1])
+console.log(flatten([[1,2,3,4],[1,[1,[1]]]]));
